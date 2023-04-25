f9a13d1233edfa6dd0a71815c1f8e63974e591c2
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-04-24 21:18:13 -0300

    Ajuste para facilitar testes.

--- java/codes/bst/bstPractice.java
+++ java/codes/bst/bstPractice.java
@@ -1,5 +1,9 @@
 public class BstPractice {
     public static void main(String[] args) {
-        
+        BST bst = new BST();
+
+        bst.insert(12);
+        bst.insert(11);
+        bst.insert(13);
     }
 }
