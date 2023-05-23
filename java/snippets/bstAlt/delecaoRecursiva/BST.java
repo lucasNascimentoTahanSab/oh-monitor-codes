55d229366daf2f930006e15ca875e9badb05f430
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-05-23 12:36:45 -0300

    Ajustes.

--- java/snippets/bstAlt/delecaoRecursiva/BST.java
+++ java/snippets/bstAlt/delecaoRecursiva/BST.java
@@ -15,84 +15,169 @@ public class BST {
 
   public BST() {
     this.raiz = null;
+
+    this.initialize();
   }
 
-  /**
-   * Método responsável pela inserção de uma nova chave em
-   * árvore binária de busca a partir da raiz.
-   */
   public void inserir(Integer chave) {
-    this.raiz = inserir(this.raiz, chave);
+    this.raiz = inserir(null, this.raiz, chave);
+
+    this.walk(this.raiz, null);
   }
 
-  /**
-   * Método responsável por caminhar recursivamente pela BST
-   * para a inserção de uma nova chave.
-   */
-  private Node inserir(Node no, Integer chave) {
-    if (no == null) { // Quando nó nulo,
-      return new Node(chave); // insira.
+  private Node inserir(Node pai, Node filho, Integer chave) {
+    if (filho == null) {
+      if (pai != null) { this.walk(pai, null); }
+
+      filho = new Node(chave);
+
+      this.insert(filho);
+
+      return filho;
     }
 
-    if (chave < no.chave) { // Quando menor que o nó atual,
-      no.esquerda = inserir(no.esquerda, chave); // insira à esquerda.
-    } else if (chave > no.chave) { // Quando maior que o nó atual,
-      no.direita = inserir(no.direita, chave); // insira à direita.
+    this.walk(pai, filho);
+
+    if (chave < filho.chave) {
+      filho.esquerda = inserir(filho, filho.esquerda, chave);
+
+      this.walk(filho.esquerda, filho);
+    } else if (chave > filho.chave) {
+      filho.direita = inserir(filho, filho.direita, chave);
+
+      this.walk(filho.direita, filho);
     }
 
-    return no;
+    return filho;
   }
 
-  /**
-   * Método responsável pela deleção da chave recebida em
-   * árvore binária de busca a partir da raiz.
-   */
   public void deletar(Integer chave) {
-    this.raiz = this.deletar(this.raiz, chave);
+    this.raiz = this.deletar(this.raiz, this.raiz, chave);
+
+    walk(this.raiz, null);
   }
 
-  /**
-   * Método responsável por caminhar recursivamente pela BST
-   * para remoção da chave recebida.
-   */
-  public Node deletar(Node no, Integer chave) {
-    if (no == null) { // Quando chave não encontrada,
-      return no; // retorne.
+  public Node deletar(Node pai, Node no, Integer chave) {
+    if (no == null) {
+      return no;
     }
 
-    if (chave < no.chave) { // Quando chave menor que a chave do nó atual,
-      no.esquerda = this.deletar(no.esquerda, chave); // caminhe para a esquerda.
-    } else if (chave > no.chave) { // Quando chave maior que a chave do nó atual,
-      no.direita = this.deletar(no.direita, chave); // caminhe para a direita.
-    } else { // Quando encontrada a chave,
-      if (no.esquerda == null) { // se não houver filho à esquerda,
-        return no.direita; // assuma o filho à direita,
+    walk(pai, no);
+
+    if (chave < no.chave) {
+      no.esquerda = this.deletar(no, no.esquerda, chave);
+
+      walk(no.esquerda, no);
+    } else if (chave > no.chave) {
+      no.direita = this.deletar(no, no.direita, chave);
+
+      walk(no.esquerda, no);
+    } else {
+      if (no.esquerda == null) {
+        walk(no, no.direita);
+        delete(no);
+
+        return no.direita; 
       }
 
-      if (no.direita == null) { // se não houver filho à direita,
-        return no.esquerda; // assuma o filho à esquerda.
+      if (no.direita == null) { 
+        walk(no, no.esquerda);
+        delete(no);
+
+        return no.esquerda;
       }
 
-      // Quando houverem ambos os filhos,
+      walk(no, no.direita);
 
-      Node substituto = this.minimo(no.direita); // assuma o menor à direita.
+      Node substituto = this.minimo(no.direita);
+
+      update(no, substituto.chave);
 
       no.chave = substituto.chave;
-      no.direita = this.deletar(no.direita, no.chave); // Remova o menor à direita.
+      no.direita = this.deletar(no, no.direita, no.chave);
+
+      walk(no.direita, no);
     }
 
     return no;
   }
 
-  /**
-   * Método responsável pela obtenção do nó de menor chave a partir do nó
-   * recebido.
-   */
   public Node minimo(Node no) {
     while (no.esquerda != null) {
+      walk(no, no.esquerda);
+
       no = no.esquerda;
     }
 
     return no;
+  }
+
+  private void update(Node no, int novoValor) {
+    System.out.print("subject");
+    System.out.print("/");
+    System.out.print(
+      "{ " +
+        "\"structure\": \"" + System.identityHashCode(this) + "\", " +
+        "\"address\": \"" + System.identityHashCode(no) + "\", " +
+        "\"old\": " + no.chave + ", " +
+        "\"new\": " + novoValor + ", " +
+        "\"operation\": \"update\" " +
+      "}"
+    );
+    System.out.print("\n");
+  }
+
+  private void delete(Node no) {
+    System.out.print("subject");
+    System.out.print("/");
+    System.out.print(
+      "{ " +
+        "\"structure\": \"" + System.identityHashCode(this) + "\", " +
+        "\"address\": \"" + (no != null ? System.identityHashCode(no) : null) + "\", " +
+        "\"value\": " + (no != null ? no.chave : null) + ", " +
+        "\"operation\": \"delete\" " +
+      "}"
+    );
+    System.out.print("\n");
+  }
+
+  private void insert(Node no) {
+    System.out.print("subject");
+    System.out.print("/");
+    System.out.print(
+      "{ " +
+        "\"structure\": \"" + System.identityHashCode(this) + "\", " +
+        "\"address\": \"" + (no != null ? System.identityHashCode(no) : null) + "\", " +
+        "\"value\": " + (no != null ? no.chave : null) + ", " +
+        "\"operation\": \"insert\" " +
+      "}"
+    );
+    System.out.print("\n");
+  }
+
+  private void walk(Node origin, Node destiny) {
+    System.out.print("subject");
+    System.out.print("/");
+    System.out.print(
+      "{ " +
+        "\"structure\": \"" + System.identityHashCode(this) + "\", " +
+        "\"origin\": " + (origin != null ? origin.chave : null) + ", " +
+        "\"destiny\": " + (destiny != null ? destiny.chave : null) + ", " +
+        "\"operation\": \"walk\" " +
+      "}"
+    );
+    System.out.print("\n");
+  }
+
+  private void initialize() {
+    System.out.print("subject");
+    System.out.print("/");
+    System.out.print(
+      "{ " +
+        "\"address\": \"" + System.identityHashCode(this) + "\", " +
+        "\"operation\": \"initialize\" " +
+      "}"
+    );
+    System.out.print("\n");
   }
 }
