42a93a47c539948d6de28b44bba1dd9a74267161
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-06-04 20:21:03 -0300

    Começo de atividade prática resolvido.

--- java/codes/bst/pratica/Main.java
+++ java/codes/bst/pratica/Main.java
@@ -1,5 +1,23 @@
 public class Main {
+  /**
+   * Método responsável pela montagem das prioridades recebidas como entrada
+   * em BST.
+   */
+  private static BST obterPrioridadesOrdenadas(String[] args) {
+    BST prioridades = new BST();
+
+    for (String arg : args) {
+      prioridades.inserir(Integer.valueOf(arg));
+    }
+
+    return prioridades;
+  }
+
   public static void main(String[] args) {
+    BST prioridades = obterPrioridadesOrdenadas(args);
+
+    prioridades.imprimirInOrdem();
+
     // Sua solução vem aqui!
   }
 }
