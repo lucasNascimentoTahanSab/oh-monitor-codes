75fd58da2eb4fe668ffcb65ee6919f3e57e6b765
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-06-04 21:06:29 -0300

    Ajuste para leitura de STD input.

--- java/codes/bst/pratica/Main.java
+++ java/codes/bst/pratica/Main.java
@@ -1,20 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+  private static Scanner scanner = new Scanner(System.in);
+
   /**
    * Método responsável pela montagem das prioridades recebidas como entrada
    * em BST.
    */
-  private static BST obterPrioridadesOrdenadas(String[] args) {
+  private static BST obterPrioridadesOrdenadas() {
     BST prioridades = new BST();
 
-    for (String arg : args) {
-      prioridades.inserir(Integer.valueOf(arg));
+    while (scanner.hasNextLine()) {
+      prioridades.inserir(Integer.valueOf(scanner.nextLine()));
     }
 
     return prioridades;
   }
 
   public static void main(String[] args) {
-    BST prioridades = obterPrioridadesOrdenadas(args);
+    BST prioridades = obterPrioridadesOrdenadas();
 
     prioridades.imprimirInOrdem();
 
